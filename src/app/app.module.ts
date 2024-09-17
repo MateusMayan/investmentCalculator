6938608a14124f09a2af233b64bad99e928c7194
Mateus Mayan <mateusmayan@icloud.com>
2024-09-17 16:37:10 -0300

    refactor(app): converte componente App de standalone para módulo

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
