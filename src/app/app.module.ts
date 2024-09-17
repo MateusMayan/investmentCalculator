6e7a81c5ff3e5bb85867ce060af80db9c2c4161b
Mateus Mayan <mateusmayan@icloud.com>
2024-09-17 17:21:05 -0300

    feat: added user-input component to App Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from "./user-input/user-input.component";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HeaderComponent, UserInputComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
