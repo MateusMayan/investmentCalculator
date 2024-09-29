d423b68b3187826cbfa42ad927a6281bfd35dc89
Mateus Mayan <mateusmayan@icloud.com>
2024-09-29 19:11:02 -0300

    feat(userInput): improves code legibility using @Output

--- src/app/investment-input.model.ts
+++ src/app/investment-input.model.ts
@@ -1,4 +1,4 @@
-export interface InvestmenteInput {
+export interface InvestmentInput {
     initialInvestment: number;
     annualInvestment: number;
     expectedReturn: number;
--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,22 +1,24 @@
-import { Component } from '@angular/core';
-import { calculateInvestmentResults } from '../investment-results';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
-  initialInvestment: number = 0
-  annualInvestment: number = 0
-  expectedReturn: number = 0
-  duration: number = 0
-  annualData = {}
-  
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+  initialInvestment: number = 0;
+  annualInvestment: number = 0;
+  expectedReturn: number = 5;
+  duration: number = 10;
 
   onSubmit() {
-    this.annualData = calculateInvestmentResults(this.initialInvestment, this.annualInvestment, this.expectedReturn, this.duration)
-    console.log(this.annualData);
-    
+    this.calculate.emit({
+      initialInvestment: +this.initialInvestment,
+      annualInvestment: +this.annualInvestment,
+      expectedReturn: +this.expectedReturn,
+      duration: +this.duration,
+    });
   }
- }
+}
