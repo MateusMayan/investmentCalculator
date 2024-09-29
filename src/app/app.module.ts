4300da8c326a4e4edb1050157f91362262e32c5b
Mateus Mayan <mateusmayan@icloud.com>
2024-09-29 19:14:24 -0300

    feat(App): added InvestmentResultsComponent in AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from "./user-input/user-input.component";
+import { UserInputComponent } from './user-input/user-input.component';
 import { FormsModule } from '@angular/forms';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UserInputComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserInputComponent,
+    InvestmentResultsComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
